1cbd986d24c0a09fd5ff40216a9d10fdb13b1299
Beatrice Mendoza <mendoza@nyu.edu>
2018-10-03 03:56:07 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,11 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
   BRANDS = []
   
   def initialize(brand)
     @brand = brand
-  end
-  
-  def brand
     BRANDS << brand
   end
 
